adc545d822f112fb4bfecf9bd770a98cda3b8468
Mohamed Hasan Musaraf <hasanmusaraf786@gmail.com>
2021-11-13 16:05:23 +0530

    added newfile

---
+++ src/pages/final main src/main.js
@@ -0,0 +1,25 @@
+function teacher() {
+    let x = document.getElementById('teach').value;
+
+    if (x === 'A0001') {
+        window.open('/pages/Chitra.html')
+    }else if (x === 'A0002') {
+        window.open('/pages/ragu.html')
+    }else if (x === 'A0003') {
+        window.open('/pages/revathi.html')
+    }else if (x === 'A0004') {
+        window.open('/pages/selvi.html')
+    }else if (x === 'A0005') {
+        window.open('/pages/staff5.html')
+    }else if (x === 'A0006') {
+        window.open('/pages/staff6.html')
+    }else if (x === 'A0007') {
+        window.open('/pages/staff7.html')
+    }else if (x === 'A0008') {
+        window.open('/pages/ragu1.html')
+    }
+    else {
+        alert('Please Check Your ID')
+    }
+    
+}
